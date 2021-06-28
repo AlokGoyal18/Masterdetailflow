bc199381c82b590af6831864a8ba298eedf6df75
ALOK GOYAL <alok.goyal1894@gmail.com>
2021-06-28 11:23:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/masterdetailflow/placeholder/PlaceholderContent.kt
@@ -0,0 +1,64 @@
+package com.example.masterdetailflow.placeholder
+
+import android.os.Environment
+import java.io.File
+import java.util.ArrayList
+import java.util.HashMap
+
+/**
+ * Helper class for providing sample content for user interfaces created by
+ * Android template wizards.
+ *
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+object PlaceholderContent  {
+
+    /**
+     * An array of sample (placeholder) items.
+     */
+    val ITEMS: MutableList<PlaceholderItem> = ArrayList()
+
+    /**
+     * A map of sample (placeholder) items, by ID.
+     */
+    val ITEM_MAP: MutableMap<String, PlaceholderItem> = HashMap()
+
+
+    private val loc= Environment.getRootDirectory()
+    private val root = File(loc.toURI())
+    private val dirs: Array<out File>? =  root.listFiles()
+
+    private val COUNT = dirs?.count()
+
+    init {
+        // Add some sample items.
+        for (i in 1..COUNT!!) {
+            addItem(createPlaceholderItem(i-1))
+        }
+    }
+
+    private fun addItem(item: PlaceholderItem) {
+        ITEMS.add(item)
+        ITEM_MAP[item.id] = item
+    }
+
+    private fun createPlaceholderItem(position: Int): PlaceholderItem {
+        return PlaceholderItem(position.toString(), "${dirs?.get(position)?.name}", makeDetails(position))
+    }
+
+    private fun makeDetails(position: Int): String {
+        val builder = StringBuilder()
+        builder.append("Details about Item: ").append(position)
+        for (i in 0 until position) {
+            builder.append("\nMore details information here.")
+        }
+        return builder.toString()
+    }
+
+    /**
+     * A placeholder item representing a piece of content.
+     */
+    data class PlaceholderItem(val id: String, val content: String, val details: String) {
+        override fun toString(): String = content
+    }
+}
